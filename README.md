72e1cf614aa96792b80eec512a184d8a57b43ac7
lukang <lukang8596@outlook.com>
2017-11-18 10:06:44 +0800

    done

---
+++ README.md
@@ -0,0 +1,26 @@
+# ikan-movie
+
+- 项目一期基本功能已经实现。
+- 唯一bug：list页面不能修改数据。
+
+## 运行项目：
+
+拉取到本地后：
+
+- bower install
+- npm install
+- node app.js
+
+```
+//首页
+localhost:3000
+//后台页
+localhost:3000/admin/movie
+//列表页
+localhost:3000/admin/list
+```
+
+## 数据库
+
+- 数据库名：ikan-movie
+- 项目根目录下的ikan-movie.json 是伪造好一条数据，可直接导入数据库，你也可以进入后台手动添加数据。
--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var express = require('express')  
+var express = require('express')
+
+//express的中间件，旧版本express内置了此模块，新版express需要单独安装。
+//此中间件用于格式化表单提交的数据
 var bodyParser = require('body-parser')
-var app = express()  
+
+//创建一个exress实例
+var app = express()
+
+//node内置的模块
 var path = require('path')
+//日期和时间格式化插件，在list页面中格式化日期
 app.locals.moment = require('moment');
 
 //引入mongoose模块，来连接数据库
@@ -9,8 +17,10 @@ var mongoose = require('mongoose')
 //引入字段替换模块
 var _ = require('underscore')
 
-//加载movie模型
+//加载Movie模型
 var Movie = require('./models/movie')
+
+//设置默认端口3000，并可以从命令行传端口号参数
 var port = process.env.PORT||3000  
 
 //连接本地的数据库，数据库名：ikan-movie
@@ -19,16 +29,21 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     console.log("ikan-movie db is connected")
-    // we're connected!
 });
 
+//设置页面试图入口文件夹
 app.set('views','./views/pages')
+//设置试图引擎模板
 app.set('view engine','jade')
 
+//格式化表单提交数据
 app.use(bodyParser.urlencoded({ extended: true }))
+
+//通过node内置的path模块，设置公共静态资源目录
 app.use(express.static(path.join(__dirname + '/public')))
 
-app.listen(port, () => {
+//监听端口
+app.listen(port, ()=>{
     console.log('ikan-movie start on port:' + port)
 })
 
@@ -51,6 +66,9 @@ app.get("/",function(req,res){
 
 //请求详情页
 // 斜杠+id,这样可以通过 req.params.id 拿到id的值
+//get：params
+//post:body
+//delete:delete
 app.get("/movie/:id", function (req, res) {
     var id = req.params.id
     Movie.findById(id,function(err,movie){
@@ -119,14 +137,14 @@ app.post('/admin/movie/new',function(req,res){
 
 //这里有bug，不能修改已经存在的数据。
 //未解决bug
-    if (id !== 'undefined'){
+    if (id !== undefined){
         Movie.findById(id,function(err,movie){
             if(err){
                 console.log(err)
             }
 // 用post过来的电影数据替换掉旧的数据
 //underscope的extend方法，用新对象的字段来替换掉对应的旧字段
-//查询到的参数放在1位置，将要 post的参数放在第二个位置
+//查询到的参数放在第一个位置，将要 post的参数放在第二个位置
             _movie = _.extend(movie,movieObj)
             
             //里面的回调中的movie是save后的
@@ -139,7 +157,7 @@ app.post('/admin/movie/new',function(req,res){
         })
     }
     else{
-        _movie = new movie({
+        _movie = new Movie({
             doctor: movieObj.doctor,
             title: movieObj.title,
             country: movieObj.country,
